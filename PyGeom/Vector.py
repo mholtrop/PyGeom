5cab520b4e5bbf0014070404e9e023cb8bdef46b
Maurik Holtrop <maurik@physics.unh.edu>
2019-04-10 13:43:34 -0400

    Avoid the circular import of Rotation in Vector. Not needed, since isinstance(data,np.matrix) is sufficient.

--- PyGeom/Vector.py
+++ PyGeom/Vector.py
@@ -6,12 +6,10 @@
 import numpy as np
 import math
 
-from . import Rotation
-
 class Vector(np.matrix):
-    """ A class to simplify the matrix objects that behave as 3 vectors, i.e. a 1-d column matrix. 
+    """ A class to simplify the matrix objects that behave as 3 vectors, i.e. a 1-d column matrix.
         This is a type of vector that you can multiply by a Rotation at Rotarion*Vector"""
-    
+
     def __new__(self, data=None, dtype=None, copy=False):
         submatrix=1
         if data is not None:
@@ -28,7 +26,7 @@ class Vector(np.matrix):
                     print("Not understanding initializiation parameter: ",data)
                     raise ValueError
 
-            elif isinstance(data,Rotation.Rotation) or isinstance(data,Vector) or isinstance(data,np.matrix) or isinstance(data,np.ndarray) or isinstance(data,list) or isinstance(data,tuple):
+            elif isinstance(data,Vector) or isinstance(data,np.matrix) or isinstance(data,np.ndarray) or isinstance(data,list) or isinstance(data,tuple):
                 submatrix= np.matrix(data,dtype=dtype,copy=copy)
                 if submatrix.shape == (1,3):
                     submatrix = submatrix.T
@@ -41,7 +39,7 @@ class Vector(np.matrix):
         else:
             submatrix = np.matrix((0,0,0))
 
-        submatrix = submatrix.view(self)   # Transform matrix to Vector type.                              
+        submatrix = submatrix.view(self)   # Transform matrix to Vector type.
         return submatrix
 
     def length(self):
@@ -61,15 +59,15 @@ class Vector(np.matrix):
     def phi(self):
         """Return the azimuthal angle of the vector, i.e. angle wrt to the x axis around the z axis"""
         return math.atan2( self.item(1),self.item(0))
-    
+
     def x(self):
         """ Return the x component, or actually self.item(0) """
         return self.item(0)
-    
+
     def y(self):
         """ Return the x component, or actually self.item(1) """
         return self.item(1)
-    
+
     def z(self):
         """ Return the x component, or actually self.item(2) """
         return self.item(2)
@@ -78,15 +76,12 @@ class Vector(np.matrix):
         """ Return the list [x,y,z] """
         l = [self.item(i) for i in range(3)]
         return l
-    
-    
+
+
     def __mul__(self,other):
         """Redefined multiply calls np.matrix.__mult__ and then casts to Vector """
         out = self.view(np.matrix)*other
-        if isinstance(other,Rotation.Rotation) or isinstance(other,np.matrix):
-
+        if isinstance(other,np.matrix):
             out = out.view(Vector)
-
-
 
         return out
